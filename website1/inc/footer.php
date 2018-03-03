bd0764ab163bed826bbe58690ff8c77c82fe69f0
silverjimmy2 <silverjimmy2@gmail.com>
2018-03-03 23:41:46 +0300

    Sample web app

---
+++ website1/inc/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <p>Mywebsite &copy; 2018</p>
+</footer>
+</body>
+</html>
---
+++ website1/inc/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>MY website</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<ul>
+<li><a><a href="index.php">Home</a></li>
+<li><a><a href="about.php">About</a></li>
+<li><a><a href="contact.php">Contact</a></li>
+</ul>
