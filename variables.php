ac027a82b3b53836fc0a3c7a90f364e6e135e836
silverjimmy2 <silverjimmy2@gmail.com>
2018-03-03 12:41:08 +0300

    Php Variables

---
+++ variables.php
@@ -0,0 +1,23 @@
+<?php
+
+    $output = 'eh';
+    echo $output;
+
+    $num1 = 4;
+    $num2 = 10;
+    $sum =$num1 + $num2;
+
+    echo $sum;
+
+    $string1 = 'hello';
+    $string2 = 'world';
+
+    $greeting1 = $string1 . ' ' . $string2;
+
+    echo $greeting1;
+
+    define('GREETING', 'hello everyone');
+
+    echo GREETING;
+
+?>
